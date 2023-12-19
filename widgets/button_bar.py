03117743db423432de4bd7396566fb2c5ffdcb4e
schavan1611 <schavan3@wisc.edu>
2023-12-19 13:17:43 -0600

    comments added

--- widgets/button_bar.py
+++ widgets/button_bar.py
@@ -19,19 +19,20 @@ class ButtonBar(AnchorLayout):
                 orientation='horizontal', spacing=30, size_hint=(None, None), height=150
             )
 
+            # Iterating through the list of button information dictionaries
             for button_info in buttons:
                 button = MDFillRoundFlatButton(
-                    text=button_info['text'],
+                    text=button_info['text'], # Setting the text of the button
                     on_press=lambda _, button_info=button_info: app.switch_to_scene(button_info['target_scene_id']),
                     size_hint=(None, None),
-                    height=50,
+                    height=50, # Setting the height of the button
                     font_style='Subtitle1',
                     line_color=(0.01960784313,0.85098039215,0.90980392156,1),
                     line_width=1.1,
-                    md_bg_color=(1,0.16470588235,0.42745098039,1),
+                    md_bg_color=(1,0.16470588235,0.42745098039,1),  # Setting the background color of the button
                 )
                 button.bind(width=lambda instance, value: setattr(instance, 'text_size', (value, None)))
-                box_layout.add_widget(button)
+                box_layout.add_widget(button) # Adding the button to the BoxLayout
 
             box_layout.bind(minimum_width=box_layout.setter('width'))
             self.pos_hint = {'center_y': 0.18}
